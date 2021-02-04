84a35e4e55f912dbffaf372aae3afe74cc04c125
danst3in <50937807+danst3in@users.noreply.github.com>
2021-02-03 21:23:37 -0800

    Fight with CORS. Add resolver tests.

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const path = require("path");
+
 const fs = require("fs");
-const { ApolloServer, gql } = require("apollo-server-express");
+const {
+  ApolloServer,
+  gql,
+  addMockFunctionsToSchema,
+  makeExecutableSchema,
+} = require("apollo-server-express");
 // const { GraphQLScalarType } = require("graphql");
 const spacex_launches_data = require("./data/spacex_launches.json");
+const winston = require("winston");
+const expressWinston = require("express-winston");
 
 /**
  * //TODO:Build Schema types
@@ -21,9 +28,10 @@ const typeDefs = gql`
   ${fs.readFileSync(__dirname.concat("/schema.graphql"), "utf8")}
 `;
 
-// Resolvers define the technique for fetching the types defined in the schema.
-// Resolvers were not moved to a separate file to avoid chances of trying to import any large json file multiple times.
-// Resolver functions written as async for compatibility with live data sources.
+//* Resolvers define the technique for fetching the types defined in the schema.
+
+//* Resolvers were not moved to a separate file to avoid chances of trying to import any large json file multiple times.
+//* Resolver functions written as async for compatibility with live data sources.
 const resolvers = {
   Query: {
     launches: () => spacex_launches_data,
@@ -47,6 +55,12 @@ const resolvers = {
       });
       return launchesArr;
     },
+    readError: (parent, args, context) => {
+      fs.readFileSync("/does/not/exist");
+    },
+    authenticationError: (parent, args, context) => {
+      throw new AuthenticationError("must authenticate");
+    },
   },
   // DateTime: new GraphQLScalarType({
   //   name: "DateTime",
@@ -57,17 +71,87 @@ const resolvers = {
   // }),
 };
 
-// create express server via middleware
+// *
+// * For Testing with graphql-tools
+// const executableSchema = makeExecutableSchema({
+//   typeDefs,
+//   resolvers,
+//   logger: { log: (e) => console.log("Error from makeExecutableSchema: ", e) },
+// });
+// addMockFunctionsToSchema({
+//   schema: executableSchema,
+//   mocks: {
+// Mocks the `Int` scalar type to always return `12345`.
+//     Int: () => 12345,
+
+// Mocks the `LaunchSite` type to always return specific {object}.
+//     LaunchSite: () => ({
+//       site_id: "ccafs_slc_40",
+//       site_name: "CCAFS SLC 40",
+//       site_name_long:
+//         "Cape Canaveral Air Force Station Space Launch Complex 40",
+//     }),
+//   },
+//   preserveResolvers: false,
+// });
+
+// *
+// *
+// * create express server via middleware
 const app = express();
+
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app });
 
 const PORT = 4000;
 
+// ? experimenting with Winston for logging
+app.use(
+  expressWinston.logger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+    meta: true, // optional: control whether you want to log the meta data about the request (default to true)
+    msg: "HTTP {{req.method}} {{req.url}}", // optional: customize the default logging message. E.g. "{{res.statusCode}} {{req.method}} {{res.responseTime}}ms {{req.url}}"
+    expressFormat: true, // Use the default Express/morgan request formatting. Enabling this will override any msg if true. Will only output colors with colorize set to true
+    colorize: false, // Color the text and status code, using the Express/morgan color palette (text: gray, status: default green, 3XX cyan, 4XX yellow, 5XX red).
+    ignoreRoute: function (req, res) {
+      return false;
+    }, // optional: allows to skip some log messages based on request and/or response
+  })
+);
+
+//* Express routing
 app.use((req, res) => {
   res.status(200);
   res.send("Hello!");
   res.end();
+});
+
+//? Experimenting with Winston for error logging
+app.use(
+  expressWinston.errorLogger({
+    transports: [new winston.transports.Console()],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.json()
+    ),
+  })
+);
+
+//*
+//* My boilerplate error-handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: "Express error handler caught unknown middleware error",
+    status: 400,
+    message: { err: "An error occurred" },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
 });
 
 // The `listen` method launches a web server.
